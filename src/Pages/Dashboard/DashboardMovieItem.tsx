e57a94bbbb7209367dc4644217566e0842de9d76
BenZaad <benzaad47@gmail.com>
2024-03-09 17:08:12 +0200

    wip

--- src/Pages/Dashboard/DashboardMovieItem.tsx
+++ src/Pages/Dashboard/DashboardMovieItem.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { MovieItem } from './types';
 import { useContext } from 'react';
 import { MoviesContext } from '../../customHooks/moviesContext/MoviesContext';
@@ -6,31 +7,19 @@ import { Rating } from '../../components/ui/Rating';
 export const DashboardMovieItem = (
   feedProps: MovieItem
 ) => {
-  const {
-    id,
-    title,
-    type,
-    image,
-    largeimage,
-    synopsis,
-    rating,
-    released,
-    runtime,
-    unogsdate,
-    imdbid,
-    download,
-  } = feedProps;
+  const { id, title, rating, released, image } = feedProps;
   const { selectMovie } = useContext(MoviesContext);
 
   return (
     <div
       onClick={() => selectMovie(id)}
-      className='block bg-white border border-gray-300 rounded-md overflow-hidden shadow-md w-64 m-4 hover:shadow-lg'
+      className='block bg-white border border-gray-300 rounded-md overflow-hidden shadow-md w-64 m-4 hover:shadow-lg cursor-pointer'
+      title='Click to expand'
     >
       <img
         src={image}
         alt={title}
-        className='w-full h-auto cursor-pointer'
+        className='w-full h-auto'
       />
       <div className='p-4 flex flex-col min-h-36'>
         <h2 className='text-lg font-semibold mb-2 '>
